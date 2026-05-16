733f823c90c69c91a43a0f58c98cd69be2b393ea
Mateusz Sacha <zsuetam36@gmail.com>
2026-05-16 23:05:25 +0200

    Expanded Trustees logic

--- src/authority.py
+++ src/authority.py
@@ -1,6 +1,8 @@
-from phe import paillier
+import math
+import secrets
+from phe import paillier, EncryptedNumber
 
-from crypto_utils import canonical_json, generate_rsa_keypair, rsa_pss_sign
+from crypto_utils import canonical_json, generate_rsa_keypair, rsa_pss_sign, sha256_hex
 
 
 class AuthenticationAuthority:
@@ -19,6 +21,22 @@ class TallyAuthority:
         self.paillier_pub, self._paillier_priv = (
             paillier.generate_paillier_keypair(n_length=3072)
         )
+        self.lambda_value = math.lcm(self._paillier_priv.p - 1, self._paillier_priv.q - 1)
+        self.v = pow(self.paillier_pub.g, self.lambda_value, self.paillier_pub.nsquare)
 
-    def decrypt_tally(self, encrypted_sum):
+    def decrypt_tally(self, encrypted_sum: EncryptedNumber) -> int:
         return self._paillier_priv.decrypt(encrypted_sum)
+
+    def chaum_pedersen_prove(self, ciphertext: int) -> dict:
+        n, nsquare, g = self.paillier_pub.n, self.paillier_pub.nsquare, self.paillier_pub.g
+        k = secrets.randbelow(n - 1) + 1
+        a = pow(ciphertext, self.lambda_value, nsquare)
+        A = pow(g, k, nsquare)
+        B = pow(ciphertext, k, nsquare)
+        e = int(sha256_hex(canonical_json([g, ciphertext, self.v, a, A, B])), 16)
+        s = k - e * self.lambda_value
+        L_v = (self.v - 1) // n
+        mi = pow(L_v, -1, n)
+        m = (((a - 1) // n) * mi) % n
+
+        return {"v": self.v, "a": a, "A": A, "B": B, "e": e, "s": s, "m": m}
